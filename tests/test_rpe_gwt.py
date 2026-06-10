390fb4f786b86604254f178da59800337801b013
Louis-Jean Teitelbaum <ljt@meidosem.com>
2026-06-10 12:18:24 +0200

    feat(rpe): parse dashboard charts from getFlowsView

--- tests/test_rpe_gwt.py
+++ tests/test_rpe_gwt.py
@@ -1,4 +1,4 @@
-from lib.rpe_gwt import build_flowsview_payload, extract_frame_ids, flowsview_header
+from lib.rpe_gwt import build_flowsview_payload, extract_frame_ids, flowsview_header, parse_charts
 
 WALLET = (
     '//OK[0,1,2,["Page d\'acceuil - KPI","99b3241e","Parcours en entree","a6ca5f7d",'
@@ -32,3 +32,40 @@ def test_build_flowsview_payload_empty_list():
 def test_flowsview_header_returns_19_strings():
     sample = "7|3|19|" + "|".join(HEADER) + "|1|2|3|"
     assert flowsview_header(sample) == HEADER
+
+
+FLOWS = (
+    "//OK[7,"
+    '"Taux de recours reportea4f0ecdf31b0b23801090bd1dcb0fa0",'
+    '"Measures\\n    - Nombre de recours\\nDimensions",'
+    '"Taux de recours",'
+    '"Pr\\u00e9sence report41411590ece8ef6af71423ca28d33218",'
+    '"Measures\\n    - Acc\\u00e8s %\\n    - BRSA %\\nDimensions\\n    - C_TERRITOIRE_ID",'
+    '"/ddenterpriseapi/DDEnterpriseServlet?action\\u003DgenDocViewAD",'
+    '"Measures\\n    - Noise (%)\\nDimensions\\n    - D_DATEENT",'
+    '"Taux de recours reportea4f0ecdf31b0b23801090bd1dcb0fa0",'
+    '"Measures\\n    - Nombre de recours\\nDimensions",'
+    "7]"
+)
+
+
+def test_parse_charts_extracts_clean_records():
+    recs = parse_charts(FLOWS)
+    assert recs == [
+        {
+            "chart_title": "Taux de recours",
+            "cube_key": "ea4f0ecdf31b0b23801090bd1dcb0fa0",
+            "measures_shown": ["Nombre de recours"],
+            "dims_shown": [],
+        },
+        {
+            "chart_title": "Présence",
+            "cube_key": "41411590ece8ef6af71423ca28d33218",
+            "measures_shown": ["Accès %", "BRSA %"],
+            "dims_shown": ["C_TERRITOIRE_ID"],
+        },
+    ]
+
+
+def test_parse_charts_rejects_url_titles():
+    assert not any(r["chart_title"].startswith("/ddenterpriseapi") for r in parse_charts(FLOWS))
